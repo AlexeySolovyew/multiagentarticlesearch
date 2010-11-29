9fa0157f29b5653c3fc53d036d386870cb0bb195
artem.shutak <artem.shutak@510bf80c-23be-767b-cf18-5767923d574f>
2010-11-29 15:47:01 +0000

    Подправил класс Article. Сделал в нем возможность корректной передачи статьи как строки(toString()) и преобразование этой строки обратно в объект Article( Article(String articleAsString) ).

--- jade/src/searcher/Article.java
+++ jade/src/searcher/Article.java
@@ -1,7 +1,22 @@
 package searcher;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 public class Article {
 
+	private static final String DELIM = "&";
 	private String name;
 	private int rank;
 
@@ -17,9 +32,45 @@ public class Article {
 		rank = r;
 		name = s;
 	}
-	
-	public Article(String s) {
-		rank = 0;
-		name = s;
+
+	/**
+	 * 
+	 * @param articleAsString
+	 *            - article as string, which return method Article.toString()
+	 */
+	public Article(String articleAsString) {
+		Document document = null;
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			InputSource inputSource = new InputSource(new StringReader(
+					articleAsString));
+			document = builder.parse(inputSource);
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Element root = document.getDocumentElement();
+		NodeList nodeListName = root.getElementsByTagName("name");
+		String name = nodeListName.item(0).getTextContent();
+		this.name = name;
+		NodeList nodeListRank = root.getElementsByTagName("rank");
+		String rank = nodeListRank.item(0).getTextContent();
+		this.rank = new Integer(rank);
+	}
+
+	public String toString() {
+		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" 
+				+ "<article>"
+				+   "<name>PAGE1111</name>"
+				+   "<rank>rank10</rank>"
+				+ "</article>";
 	}
 }
