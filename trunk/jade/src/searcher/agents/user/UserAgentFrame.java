c127173afc2120f12395fb7f3f74c10a7ea4915b
alexey.solovyew@gmail.com <alexey.solovyew@gmail.com@510bf80c-23be-767b-cf18-5767923d574f>
2010-11-30 22:34:52 +0000

    Что-то типа человеческого фрейма и вывода

--- trunk/jade/src/searcher/agents/user/UserAgentFrame.java
+++ trunk/jade/src/searcher/agents/user/UserAgentFrame.java
@@ -15,8 +15,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -32,8 +34,10 @@ public class UserAgentFrame extends JFrame {
 	// private JTextArea jTextArea;
 	// private JTextField jTextField;
 	private JButton searchButton;
-	private JTextArea outputField;
+	// private JTextArea outputField;
+	private JPanel p;
 	private JTextField inputField;
+	private JTextPane outputField;
 	private List<Article> resultPages;
 
 	public UserAgentFrame(UserAgent agent) {
@@ -62,7 +66,9 @@ public class UserAgentFrame extends JFrame {
 		// this.setFrameIcon("images/dummy.gif");
 		Container container = this.getContentPane();
 		container.setLayout(new BorderLayout());
-		outputField = new JTextArea();
+		// outputField = new JTextArea();
+		p = new JPanel();
+		outputField = new JTextPane();
 		inputField = new JTextField();
 		searchButton = new JButton("search");
 		searchButton.addActionListener(new ActionListener() {
@@ -75,40 +81,60 @@ public class UserAgentFrame extends JFrame {
 		});
 
 		container.add(outputField, BorderLayout.CENTER);
-		container.add(inputField, BorderLayout.SOUTH);
-		container.add(searchButton, BorderLayout.WEST);
+		p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
+		p.add(inputField);
+		p.add(searchButton);
+		// container.add(searchButton, BorderLayout.SOUTH);
+		container.add(p, BorderLayout.SOUTH);
 	}
 
 	private void searchButton_actionPerformed() {
 		outputField.setText("");
 		resultPages.clear();
-		agent.addBehaviour( new OneShotBehaviour() {
-            public void action() {
-            	ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-        		msg.setSender(agent.getAID());
-        		msg.addReceiver(agent.getOrchestratorAID());
-        		msg.setContent(inputField.getText());
-        		msg.setLanguage("Plain English");
-        		agent.send(msg);
-            }
-        } );
+		agent.addBehaviour(new OneShotBehaviour() {
+			public void action() {
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.setSender(agent.getAID());
+				msg.addReceiver(agent.getOrchestratorAID());
+				msg.setContent(inputField.getText());
+				msg.setLanguage("Plain English");
+				agent.send(msg);
+			}
+		});
 	}
 
 	public void sortAndShow() throws InterruptedException {
-		//wait(2000);
+		// wait(2000);
 		outputField.setText("");
 		Collections.sort(resultPages, new Comparator<Article>() {
 
 			public int compare(Article o1, Article o2) {
-				return o1.getRank()-o2.getRank();
+				return o1.getRank() - o2.getRank();
 			}
 		});
-		
-		for (int i=0;i<resultPages.size();i++) {
-			outputField.append("Title="+resultPages.get(i).getTitle()+"\n"
-					+"URL="+resultPages.get(i).getURL()+"\n"
-					+"Author="+resultPages.get(i).getAuthor()+"\n"+"\n");
+
+		String s = "";
+		for (int i = 0; i < resultPages.size(); i++) {
+			/*
+			 * outputField.append("Title="+resultPages.get(i).getTitle()+"\n"
+			 * +"URL="+resultPages.get(i).getURL()+"\n"
+			 * +"Author="+resultPages.get(i).getAuthor()+"\n"+"\n");
+			 */
+			s += "<TABLE BORDER=3 WIDTH=100%>"
+					+ "<TR><TD WIDTH=200><B>Title</B></TD><TD>"
+					+ resultPages.get(i).getTitle() + "</TD></TR>"
+					+ "<TR><TD WIDTH=200><B>Author</B></TD><TD>"
+					+ resultPages.get(i).getAuthor() + "</TD></TR>"
+					+ "<TR><TD WIDTH=200><B>URL</B></TD><TD><A HREF="
+					+ resultPages.get(i).getURL() + ">"
+					+ resultPages.get(i).getURL() + "</A></TD></TR>"
+					+ "</TABLE><BR>";
+
 		}
+		outputField.setContentType("text/html");
+		//outputField.setAutoscrolls(true);
+		outputField.setEditable(false);
+		outputField.setText(s);
 
 	}
 
