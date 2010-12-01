4e3c51b9ceb793c03930124dd509d08d97f0f839
artem.shutak <artem.shutak@510bf80c-23be-767b-cf18-5767923d574f>
2010-12-01 11:53:45 +0000

    Агрегатор находит сёчеров через жёлтые страницы. Пока Уг-шно Коммит по частям(

--- jade/src/searcher/agents/aggregator/AggregatorAgent.java
+++ jade/src/searcher/agents/aggregator/AggregatorAgent.java
@@ -11,7 +11,15 @@ import searcher.agents.aggregator.AggregatorCyclicBehaviour;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
+import jade.proto.SubscriptionInitiator;
+import jade.util.leap.Iterator;
 
 public class AggregatorAgent extends Agent {
 	public static final String INIT_USER = "INIT_USER";
@@ -21,9 +29,67 @@ public class AggregatorAgent extends Agent {
 
 	protected void setup() {
 		super.setup();
-		addBehaviour(new AggregatorCyclicBehaviour(this));
+
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription templateSd = new ServiceDescription();
+		templateSd.setType("search-articles");
+
+		// тут можно будет указывать источники, по которым нужно осуществлять
+		// поиск
+		// templateSd.addProperties(new Property("country", "Italy"));
+
+		template.addServices(templateSd);
+
+		SearchConstraints sc = new SearchConstraints();
+		// We want to receive 10 results at most количество агентов,
+		// предоставляющих данный сервис, пока оставил 10
+		sc.setMaxResults(new Long(10));
+
+		addBehaviour(new SubscriptionInitiator(this,
+				DFService.createSubscriptionMessage(this, getDefaultDF(),
+						template, sc)) {
+			protected void handleInform(ACLMessage inform) {
+				System.out.println("Agent " + getLocalName()
+						+ ": Notification received from DF");
+				try {
+					DFAgentDescription[] results = DFService
+							.decodeNotification(inform.getContent());
+					if (results.length > 0) {
+						for (int i = 0; i < results.length; ++i) {
+							DFAgentDescription dfd = results[i];
+							AID provider = dfd.getName();
+							// The same agent may provide several services; we
+							// are only interested
+							// in the search-articles one
+							// не правда, конечно, но опять же пока оставил
+							Iterator it = dfd.getAllServices();
+							while (it.hasNext()) {
+								ServiceDescription sd = (ServiceDescription) it
+										.next();
+								if (sd.getType().equals("search-articles")) {
+									System.out
+											.println("Search-articles service found:");
+									System.out.println("- Service \""
+											+ sd.getName()
+											+ "\" provided by agent "
+											+ provider.getName());
+									searchersAID.add(provider);
+								}
+							}
+						}
+					}
+					System.out.println();
+				} catch (FIPAException fe) {
+					fe.printStackTrace();
+				}
+				if (searchersAID.size() == 3) {
+					myAgent.addBehaviour(new AggregatorCyclicBehaviour(
+							(AggregatorAgent) myAgent));
+				}
+			}
+		});
 	}
-	
+
 	public AID getUserAgentAID() {
 		return userAgentAID;
 	}
@@ -32,26 +98,29 @@ public class AggregatorAgent extends Agent {
 		userAgentAID = aid;
 	}
 
-	public void setSearchers(String content) {
-		for (StringTokenizer tz = new StringTokenizer(content); tz
-				.hasMoreTokens();) {
-			String searcherName = tz.nextToken();
-			searchersAID.add(new AID(searcherName, AID.ISLOCALNAME));
-		}
-	}
+	/*
+	 * public void setSearchers(String content) { for (StringTokenizer tz = new
+	 * StringTokenizer(content); tz .hasMoreTokens();) { String searcherName =
+	 * tz.nextToken(); searchersAID.add(new AID(searcherName, AID.ISLOCALNAME));
+	 * } }
+	 */
 
-	public AID getNextSearcherAID() {
-		return searchersAID.iterator().next();
-	}
+	// public AID getNextSearcherAID() {
+	// return searchersAID.iterator().next();
+	// }
 
 	public void sendArticle(Article page) {
-			ACLMessage responseMSG = new ACLMessage(ACLMessage.INFORM);
-			responseMSG.setSender(this.getAID());
-			Random random = new Random();
-			page.setRank(page.getRank()+random.nextInt(20)%20);
-			responseMSG.setContent(page.toString());
-			// responseMSG.addReceiver(this.getUserAgentAID());
-			responseMSG.addReceiver(userAgentAID);
-			this.send(responseMSG);
+		ACLMessage responseMSG = new ACLMessage(ACLMessage.INFORM);
+		responseMSG.setSender(this.getAID());
+		Random random = new Random();
+		page.setRank(page.getRank() + random.nextInt(20) % 20);
+		responseMSG.setContent(page.toString());
+		// responseMSG.addReceiver(this.getUserAgentAID());
+		responseMSG.addReceiver(userAgentAID);
+		this.send(responseMSG);
+	}
+
+	public Set<AID> getSearchersAID() {
+		return searchersAID;
 	}
 }
