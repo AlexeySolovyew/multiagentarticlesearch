e2fae6bce04be05d2b6bd77bd480f79b8f8f2491
alexey.solovyew@gmail.com <alexey.solovyew@gmail.com@510bf80c-23be-767b-cf18-5767923d574f>
2010-12-13 00:27:08 +0000

    Почти доделана работа с файлами базы данных (с точностью до записи DOM в файл) Пока закомменчена.
    P.S. Да здравствуют 10 часов мультиагентных систем!

--- trunk/jade/src/searcher/agents/udbagent/UserDataBaseAgent.java
+++ trunk/jade/src/searcher/agents/udbagent/UserDataBaseAgent.java
@@ -11,10 +11,11 @@ import java.io.StringReader;
 import java.io.Writer;
 import java.util.ArrayList;
 
+import javax.imageio.metadata.IIOMetadataNode;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.Node;
+import org.w3c.dom.*;
 
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
@@ -32,77 +33,80 @@ import jade.core.AID;
 import jade.core.Agent;
 
 public class UserDataBaseAgent extends Agent {
-	
+
 	private AID userAgentAID;
 	private AID orchestratorAgentAID;
 	private File farticles;
 	private File fauthors;
-	
 
 	public static final String INIT_USER = "INIT_USER";
-	
-	
+
 	protected void setup() {
 		super.setup();
-		initFile(farticles,"articles.xml");
-		initFile(fauthors,"authors.xml");
+		System.out.println("Вот щас бы файлики создать");
+		farticles=initFile("articles.xml");
+		fauthors=initFile("authors.xml");
+		System.out.println("И где блять ваши файлики?!?");
 		addBehaviour(new UserDataBaseAgentCyclicBehaviour(this));
 	}
-
 
 	public void setUserAID(AID aid) {
 		userAgentAID = aid;
 	}
-	
+
 	public AID getUserAgentAID() {
 		return userAgentAID;
 	}
-	
+
 	public AID getOrchestratorAID() {
 		return orchestratorAgentAID;
-		
+
 	}
 
 	public void setOrchestratorAID(AID aid) {
-		orchestratorAgentAID=aid;
-		
+		orchestratorAgentAID = aid;
+
 	}
-	
-	public void initFile(File f,String s){
-		
-	      f=new File(s);
-	      if(!f.exists()){
-	      try {
-			f.createNewFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	      System.out.println("New file "+s+" has been created to the current directory");
-		
+
+	public File initFile(String s) {
+
+		File f = new File(s);
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-	      try{
-	    	    // Create filewriter
-	    	    FileWriter fstream = new FileWriter(f);
-	    	        BufferedWriter out = new BufferedWriter(fstream);
-	    	    
-	    	    out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-	    	    out.newLine();
-	    	    out.write("<ARTICLES>");
-	    	    out.newLine();
-	    	    out.write("</ARTICLES>");
-	    	    		    	    
-	    	    //Close the output stream
-	    	    out.close();
-  	    }catch (Exception e){//Catch exception if any
-	    	      System.err.println("Error: " + e.getMessage());
-	    	    }
+			System.out.println("New file " + s
+					+ " has been created to the current directory");
+
+		}
+		System.out.println("такой файл блять уже есть: "+f.getAbsolutePath());
+		try {
+			// Create filewriter
+			FileWriter fstream = new FileWriter(f);
+			BufferedWriter out = new BufferedWriter(fstream);
+
+			out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			
+			 out.newLine(); out.write("<ARTICLES>"); out.newLine();
+			 out.write("</ARTICLES>");
+			
+
+			// Close the output stream
+			out.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+		return f;
 	}
-	
-	public void addRatings(Article a) throws DOMException, IOException{
-		
-		//просто статьи по айдишникам
-		/*Document document = null;
+
+	public void addRatings(Article a) throws DOMException, IOException {
+
+		/*
+		// просто статьи по айдишникам
+		Document document = null;
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder;
 		try {
@@ -118,50 +122,54 @@ public class UserDataBaseAgent extends Agent {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		Element root = document.getDocumentElement();
-		
+		// Допустим это <ARTICLES>
+		 Node articles = root.getChildNodes().item(0);
+
 		NodeList nl = root.getElementsByTagName("ID");
-		if (nl==null){
-			
-		}
-		
-		for (int i=0;i<nl.getLength();i++){
-			if (nl.item(i).getTextContent()==a.getURL()){
-				out.write("<ARTICLE>");
-	    	    out.newLine();
-	    	    out.write("<ID>");
-	    	    out.newLine();
-	    	    out.write(a.getURL());
-	    	    out.newLine();
-	    	    out.write("</ID>");
-	    	    out.newLine();
-	    	    out.write("<RATE>");
-	    	    out.newLine();
-	    	    out.write("1");
-	    	    out.newLine();
-	    	    out.write("</RATE>");
-	    	    out.newLine();
-	    	    out.write("</ARTICLE>");
-	    	    out.newLine();
+		if (nl.getLength() == 0) {
+			addArticle(a, document, root);
+
+		} else{
+			int i=0;
+		for (; i < nl.getLength(); i++) {
+			if (nl.item(i).getTextContent() == a.getURL()) {
+				Node rankNode = nl.item(i).getParentNode().getChildNodes().item(1);
+				//тут надо изменить ранг, это блин сложно, пока пусть будет 2
+					rankNode.setTextContent("2");
+			}
+			if (i==nl.getLength()){
+				addArticle(a, document, root);
 			}
 		}
-			
-		
+		}
+
 		Writer out = new OutputStreamWriter(new FileOutputStream(farticles));
-		    try {
-		      out.write(document.getTextContent());
-		    }
-		    finally {
-		      out.close();
-		    }
-
-		  
-			
-		
-		
-		*/
-		
+		try {
+			//тут надо бы записать нахимиченный DOM в файл (пишется вместо этого какая-то хуйня)
+			out.write(root.getElementsByTagName("ID").item(0).getTextContent());
+		} finally {
+			out.close();
+		}
+*/
 	}
-	
 
+	private void addArticle(Article a, Document document, Element root) {
+		Element art = document.createElement("ARTICLE");
+		Element elem;
+		Text elem_value;
+
+		elem = document.createElement("ID");
+		elem_value = document.createTextNode(a.getURL());
+		elem.appendChild(elem_value);
+		art.appendChild(elem);
+
+		elem = document.createElement("RATE");
+		elem_value = document.createTextNode("1");
+		elem.appendChild(elem_value);
+		art.appendChild(elem);
+
+		root.appendChild(art);
+	}
 }
