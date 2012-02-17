f549f9ab454a9df98e09b9a39b06eaf926ce09cc
alexey.solovyew <alexey.solovyew@510bf80c-23be-767b-cf18-5767923d574f>
2012-02-17 09:02:21 +0000

    -fuckyeah

---
+++ trunk/simuni.ru/files/DZ4.hs
@@ -0,0 +1,27 @@
+﻿--1 a)
+matr n = matr' n 1
+matr' n k = if k>n then [] else [makerow n k]++matr' n (k+1)
+makerow n k = makerow' n k 1
+makerow' n k m = if m>n then [] else if m==k then [1]++makerow' n k (m+1) else [0]++makerow' n k (m+1)
+--1 �)
+matrb n = matrb' n 1 n
+matrb' n k l = if k>n then [] else [makerowb n k l]++matrb' n (k+1) (l-1)
+makerowb n k l = makerowb' n k l 1
+makerowb' n k l m = if m>n then [] else if (m==k || m==l) then [1]++makerowb' n k l (m+1) else [0]++makerowb' n k l (m+1)
+--2 �)
+nechsum xs = foldr (\x res -> if x `mod` 2 == 1 then x+res else res) 0 xs
+--2 �)
+nechsum2 [] = 0
+nechsum2 (x:xs) = if x `mod` 2 == 1 then x+nechsum2 xs else nechsum2 xs
+--3
+myfoldl f e [] = e
+myfoldl f e (x:xs) = f (myfoldl f e xs) x
+--5
+comp f n = foldr . (\x -> x) (map (\x -> f) [1..n])
+ 
+
+
+
+
+
+
--- trunk/simuni.ru/www/index.php
+++ trunk/simuni.ru/www/index.php
@@ -10,10 +10,23 @@ mysql_select_db("simuni");
 
 <h2><p><b> Форма для загрузки файлов с задачами </b></p></h2>
       <form action="upload.php" method="post" enctype="multipart/form-data">
-	  Номер задачи, которую вы заливаете:
-	  <input name="tasknum"><br>
+	  Номер задачи, которую вы заливаете: <br>
+	  <?
+$tasks = mysql_query("SELECT * FROM Task");
+$q = mysql_num_rows($tasks);
+echo "<p><select size=\"1\" name=\"tasknum\">";
+echo "<option disabled>Выберите задачу</option>";
+for ( $i=0; $i<$q; $i++){
+	$row = mysql_fetch_array($tasks);
+	echo "<option value=".$row['TaskID'].">".$row['TaskID']."</option>";
+}
+echo "</select><br>";
+?>
 	  Файл с задачей: <br>
       <input type="file" name="filename"><br> 
+	  или<br>
+	  Текст задачи: <br>
+	  <input type="text" maxlength="500" name="code"><br>
       <input type="submit" value="Загрузить и начать тестирование"><br>
       </form>
 <br>
